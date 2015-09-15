eab61e26f998faaac1b739f469a59cef3844d337
TruemenHale <truemenhale@Hales-MacBook-Pro.local>
2015-09-15 23:11:16 +0800

    ajax

--- Public/JS/main.js
+++ Public/JS/main.js
@@ -5,15 +5,22 @@ $(function(){
 	var oMask = $('.mask');
 	var oSelector = $('.schoolSelector');
 	var aSeBox = $('.schoolSelector li');
-	$(".selectorBtn").on('tap',function(){
+	$(".selectorBtn").on('click',function(){
 		oMask.css('z-index',999);
 		oSelector.css('z-index',1000);
+		oSelector.animate({"top":"60%"});
 	});
 	oMask.on('tap',function(){
-		oMask.css('z-index',-999);
-		oSelector.css('z-index',-1000);
+		oSelector.animate({"top":"100%"},function(){
+			oMask.css('z-index',-999);
+			oSelector.css('z-index',-1000);
+		});
 	});
 	aSeBox.on('tap',function(){
-
+		var uName = $(this).html();
+		$.mobile.changePage('#single',{
+			transition:"flow"
+		});
+		$('.uName').html(uName);
 	})
 });
