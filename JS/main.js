ced0695474b41f602d834797487f4b84dd1e865e
TruemenHale <truemenhale@Hales-MacBook-Pro.local>
2015-09-15 16:18:47 +0800

    single

--- JS/main.js
+++ JS/main.js
@@ -2,8 +2,18 @@
  * Created by truemenhale on 15/9/15.
  */
 $(function(){
+	var oMask = $('.mask');
+	var oSelector = $('.schoolSelector');
+	var aSeBox = $('.schoolSelector li');
 	$(".selectorBtn").on('tap',function(){
-		$('.mask').css('z-index',999);
-		$('.schoolSelector').css('z-index',1000);
+		oMask.css('z-index',999);
+		oSelector.css('z-index',1000);
 	});
+	oMask.on('tap',function(){
+		oMask.css('z-index',-999);
+		oSelector.css('z-index',-1000);
+	});
+	aSeBox.on('tap',function(){
+
+	})
 });
---
+++ JS/single.js
@@ -0,0 +1,8 @@
+/**
+ * Created by truemenhale on 15/9/15.
+ */
+$(function(){
+	$('.backRow').on('tap',function(){
+		$.mobile.changePage('http://localhost:63342/Charman/index.html');
+	});
+});
