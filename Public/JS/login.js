ea7c269e1b4afe5d9dac2fcc07d513e6f679d7c9
TruemenHale <truemenhale@Hales-MacBook-Pro.local>
2015-09-16 00:38:28 +0800

    login

---
+++ Public/JS/login.js
@@ -0,0 +1,24 @@
+/**
+ * Created by truemenhale on 15/9/15.
+ */
+$(function(){
+	$('#loginBtn').on('tap',function(){
+		$.mobile.loading('show');
+		var _usr = {};
+		_usr.usrname = $('#usrName');
+		_usr.password = $('#password');
+		_usr = JSON.stringify(_usr);
+		$.post('url',_usr,function(data){
+			if(data.status == 200){
+				$.mobile.loading('hide');
+				$.mobile.changePage();//单独页面还没写完
+			}
+			else if(data.status == 403){
+				alert('用户名或密码错误！');
+			}
+			else{
+				alert('服务器连接失败！');
+			}
+		});
+	})
+});
