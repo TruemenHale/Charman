2629feb175340db91092b086585456df25f81a3a
Lich2013 <longzy2013@gmail.com>
2015-09-17 20:44:31 +0800

    test

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -3,6 +3,8 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
+        $data = M('school')->field('school_name')->select();
+        $this->assign('data', $data);
         $this->display();
     }
 }
