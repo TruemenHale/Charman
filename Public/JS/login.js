56cb203c021d8a1faef77b4f3b1cc0f7eb17e6f7
TruemenHale <truemenhale@Hales-MBP.lan>
2015-09-20 16:51:34 +0800

    login

--- Public/JS/login.js
+++ Public/JS/login.js
@@ -3,21 +3,20 @@
  */
 $(function(){
 	$('#loginBtn').on('tap',function(){
-		$.mobile.loading('show');
 		var _usr = {};
 		_usr.username = $('#usrName').val();
 		_usr.password = $('#password').val();
-		if(_usr.usrname && _usr.password){
+		if(_usr.username && _usr.password){
+			$.mobile.loading('show');
 			$.post(login_path,_usr,function(data){
 				if(data.status == 200){
 					$.mobile.loading('hide');
-					$.mobile.changePage('#');
-				}
-				else if(data.status == 403){
-					alert('用户名或密码错误！');
+					$.mobile.changePage('#commentInfo',{
+						transition:'flow'
+					});
 				}
 				else{
-					alert('服务器连接失败！');
+					alert(data.info);
 				}
 			});
 		}
