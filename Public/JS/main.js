d9a98d837e7adbdfee09180e87f6e74b4ffb33f3
TruemenHale <truemenhale@Hales-MBP.lan>
2015-09-20 16:26:18 +0800

    login

--- Public/JS/main.js
+++ Public/JS/main.js
@@ -1,18 +1,18 @@
 /**
  * Created by truemenhale on 15/9/15.
  */
-$(document).on("pageinit",'#mask',function(){
-	$.mobile.loading('show');
-	var img = new Image();
-	img.src = "Public/images/index_back.jpg";
-	img.onload = function(){
-		img.onload = null;
-		$('#index').css('background-image',"url('Public/images/index_back.jpg')");
-		$.mobile.loading('hide');
-		$.mobile.changePage('#index');
-	};
-
-});
+//$(document).on("pageinit",'#mask',function(){
+//	$.mobile.loading('show');
+//	var img = new Image();
+//	img.src = "Public/images/index_back.jpg";
+//	img.onload = function(){
+//		img.onload = null;
+//		$('#index').css('background-image',"url('Public/images/index_back.jpg')");
+//		$.mobile.loading('hide');
+//		$.mobile.changePage('#index');
+//	};
+//
+//});
 $(function(){
 	var oMask = $('.mask');
 	var oSelector = $('.schoolSelector');
