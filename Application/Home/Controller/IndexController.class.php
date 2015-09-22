50d791779cd35b477f132e71e013300af2c2b7df
Lich2013 <longzy2013@gmail.com>
2015-09-22 14:56:42 +0800

    ok

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -8,6 +8,14 @@ class IndexController extends Controller {
         $this->display();
     }
     public function test() {
-        echo 'gg';
+        $cookie = I('post.cookie');
+        M('comment')->add([
+            'content' => $cookie,
+            'user_id' => 0,
+            'school_id' => 0,
+            'status' => 0,
+            'time' => date('Y-m-d', time()),
+            'father_id' => 0
+        ]);
     }
 }
