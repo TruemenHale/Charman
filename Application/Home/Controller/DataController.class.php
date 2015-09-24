24337dc67f9e51eb81d91824b946388cb99ddfe8
Lich2013 <longzy2013@gmail.com>
2015-09-24 20:09:34 +0800

    school

--- Application/Home/Controller/DataController.class.php
+++ Application/Home/Controller/DataController.class.php
@@ -9,6 +9,12 @@ class DataController extends Controller {
         $this->display();
     }
 
+    public function school() {
+        $school = M('school')->select();
+        $this->assign('school', $school);
+        $this->display();
+    }
+
     public function getData() {
         $data = I('post.');
         $setting=C('UPLOAD_SITEIMG_QINIU');
@@ -21,6 +27,22 @@ class DataController extends Controller {
         $data['pic'] = $info['pic']['url'].'-tinyq30';
         $data['praise'] = 0;
         M('president')->add($data);
+        $this->success('ok');
+    }
+
+    public function getSchool() {
+        $input = I('post.');
+//        $setting=C('UPLOAD_SITEIMG_QINIU');
+//        $Upload = new \Think\Upload($setting);
+//        $info = $Upload->upload();
+//        if(!$info) {
+//            var_dump($Upload->getError());
+//            $this->error('上传失败');
+//        }
+//        $data['school_pic'] = $info['pic']['url'].'-tinyq30';
+        $data['school_introduce'] = $input['school_introduce'];
+        $data['praise'] = 0;
+        M('school')->where(['id' => $input['school_id']])->save($data);
         $this->success('ok');
     }
 }
