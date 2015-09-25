6d83cb18dc41fb869ad9cfd2b4aa3a2736646e2d
Lich2013 <longzy2013@gmail.com>
2015-09-25 22:21:02 +0800

    base

--- Application/Home/Controller/UserBaseController.class.php
+++ Application/Home/Controller/UserBaseController.class.php
@@ -3,7 +3,7 @@ namespace Home\Controller;
 use Think\Controller;
 class UserBaseController extends Controller {
     public function _initialize(){
-//        if(!session('uid')) {
+        if(!session('uid')) {
             $tools = new ToolController();
             $userInfo = $tools->GetOpenid();
             if($userInfo) {
@@ -13,17 +13,7 @@ class UserBaseController extends Controller {
             if($users->where(['openid' => $userInfo['openid']])->count()) {
                 $uid = $users->where(['openid' => $userInfo['openid']])->getField('id');
                 session('uid', $uid);
-            } else {
-                $data = [
-                    'openid' => $userInfo['openid'],
-                    'nickname' => $userInfo['nickname'],
-                    'avatar' => $userInfo['headimg'],//todo
-                    'role_id' => 1,
-                    'password' => ''
-                ];
-                $uid = $users->add($data);
-                session('uid', $uid);
             }
-//        }
+        }
     }
 }
