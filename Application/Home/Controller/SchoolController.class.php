cde96c3198215bd62a603df47559200e5b6fe374
Lich2013 <longzy2013@gmail.com>
2015-10-12 17:11:35 +0800

    ok

--- Application/Home/Controller/SchoolController.class.php
+++ Application/Home/Controller/SchoolController.class.php
@@ -1,7 +1,7 @@
 <?php
 namespace Home\Controller;
 use Think\Controller;
-class SchoolController extends Controller {
+class SchoolController extends UserBaseController {
     public function index(){
         $this->display();
     }
--- Application/Home/Controller/SeduceController.class.php
+++ Application/Home/Controller/SeduceController.class.php
@@ -1,7 +1,7 @@
 <?php
 namespace Home\Controller;
 use Think\Controller;
-class SeduceController extends Controller {
+class SeduceController extends UserBaseController {
 
     //勾搭首页
     public function index() {
