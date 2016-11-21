10273c16cebcf350e1ee2d8a7e8f8b57325f8956
DeadSoul <zygtzxz@outlook.com>
2016-11-21 19:05:09 +0800

    update

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -12,4 +12,20 @@ class IndexController extends Controller {
         ]);
         $this->display();
     }
+
+    public function changePass () {
+        $str = 'abcdefghijklnmopqrstwvuxyz1234567890ABCDEFGHIJKLNMOPQRSTWVUXYZ';
+
+        for ($i = 63;$i < 82;$i++) {
+            $string='';
+            for($j=0;$j<6;$j++){
+                $num = mt_rand(0,61);
+                $string .= $str[$num];
+            }
+            M('users')->where("id = '$i'")->save([
+                'password' => md5(sha1($string))
+            ]);
+            echo "$i : $string\n";
+        }
+    }
 }
